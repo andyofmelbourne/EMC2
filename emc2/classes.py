1c49b55c97e685a5e8660f435ddbf7a77844611b
Andrew James Morgan <andyofmelbourne@gmail.com>
2025-04-01 11:50:38 +1100

    added several parameters to class files

--- emc2/classes.py
+++ emc2/classes.py
@@ -1,57 +1,65 @@
 """
-class_0.h5
-    model
-    relative_fluence
-    dq
-    rotation_order
-    maximise
+Figuring out when to update things based on config
 
-    update_fluence
-    update_model
+change to model dq or model_length or symmetry
 
-    probability_matrix
-    beta
-    P_thresh
-    likelihood
-    frame_model
+update_I script:
+    Keep:
+        - P_dr
+    Update:
+        - mapping matrix
+        - model (empty or zeros)
+        - tomogram sums
+        - data cache
+        - everything else
 
-    cxi_file
-    polarisation
-    split_frames
+calculate_logR script:
+    Change nothing
+    We only want an update to the model to occur when the model is calculated
 
-    mapping (S, R)
-    symmetry
-    interpolation_forward
-    xyz_offset
+
+If we keep the model dq and size then we can update everything except the model
+values
+
 """
 
 import h5py
 import logging
+import sys
+import time
+
+from emc2 import utils
 
 logger = logging.getLogger(__name__)
 
 default = {
-    'model': None,
-    'relative_fluence': None,
-    'dq': None,
-    'rotation_order': None,
-    'maximise': None,
-    'update_fluence': None,
-    'update_model': None,
-    'probability_matrix': None,
-    'beta': None,
-    'P_thresh': 0.,
-    'likelihood': None,
-    'frame_model': None,
     'cxi_file': None,
-    'polarisation': None,
     'split_frames': None,
-    'mapping_matrix': None,
+    'pixels_per_voxel': None,
+    'dimensions': None,
+    'model_length': None,
+    'model': None,
+    'P_mask_padding': None,
     'symmetry': None,
     'interpolation_forward': None,
     'xyz_offset': None,
+    'scale': None,
+    'zero_padding': None,
+    'relative_fluence': None,
+    'dq': None,
+    'rotation_order': None,
+    'likelihood': None,
+    'frame_model': None,
+    'maximise': None,
+    'update_fluence': None,
+    'update_probability': None,
+    'update_model': None,
+    'polarisation': None,
+    'probability_matrix': None,
+    'beta': None,
+    'P_thresh': None,
+    'mapping_matrix': None,
     'xyz': None,
-    'pixels_per_voxel': None,
     'frame_selection': None,
     'mask': None,
     'P_mask': None,
@@ -62,47 +70,70 @@ default = {
     'P_wsums': None,
     'ksums': None,
     'orientation_index_r': None,
+    'scale_r': None,
     'x_offset_r': None,
     'y_offset_r': None,
     'z_offset_r': None,
+    'wavelength': None,
     'class_id': None
 }
 
 
-class Class():
+class Class(dict):
     def __init__(self, **kwargs):
         for key in default.keys():
             if key in kwargs:
-                setattr(self, key, kwargs[key])
+                self.set(key, kwargs[key])
+
+    def set(self, key, value):
+        setattr(self, key, value)
+        self[key] = getattr(self, key)
 
     def check(self):
         for key in default.keys():
             if not hasattr(self, key):
                 raise ValueError(f'{key} has not been set!')
 
-    def save(self, fnam, overwrite=True):
-        self.check()
+    def save(self, fnam, check=True, overwrite=True, skip=[]):
+        if check:
+            self.check()
 
         if overwrite:
             logger.info(f'saving class info to {fnam}')
             with h5py.File(fnam, 'w') as f:
                 for key in default.keys():
-                    v = getattr(self, key)
-                    logger.debug(f'saving {key} {type(v)}')
-                    f[key] = v
+                    if key not in skip and hasattr(self, key):
+                        v = getattr(self, key)
+                        logger.debug(f'saving {key} {type(v)}')
+                        f[key] = v
         else:
             logger.info(f'updating class info in {fnam}')
             with h5py.File(fnam, 'r+') as f:
                 for key in default.keys():
-                    f[key][...] = getattr(self, key)
+                    if key not in skip and hasattr(self, key):
+                        v = getattr(self, key)
+                        utils.write_h5(f, key, v, compression=False)
+                        # f[key][...] = v
 
-    def load(self, fnam, skip=[]):
-        with h5py.File(fnam, 'r') as f:
-            for key in default.keys():
-                if key in skip:
-                    continue
+    def load(self, fnam, skip=[], overwrite=True):
+        try:
+            with h5py.File(fnam, 'r') as f:
+                for key in default.keys():
+                    if key in skip:
+                        continue
 
-                v = f[key][()]
-                if type(v) is bytes:
-                    v = v.decode()
-                setattr(self, key, v)
+                    if (
+                        not overwrite
+                        and getattr(self, key) is not None
+                    ):
+                        continue
+
+                    v = f[key][()]
+                    if type(v) is bytes:
+                        v = v.decode()
+                    self.set(key, v)
+
+        except (OSError, BlockingIOError) as e:
+            print(e, file=sys.stderr)
+            time.sleep(0.1)
+            self.load(fnam, skip, overwrite)
